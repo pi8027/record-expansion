2d79930dffcbec47754011d8d078f07aa2d7ca3e
Kazuhiko Sakaguchi <pi8027@gmail.com>
2023-03-13 14:43:36 +0100

    Define a generic datatype representing expansions

--- theories/record_expansion.v
+++ theories/record_expansion.v
@@ -1,61 +1,61 @@
 From elpi Require Import elpi.
-From record_expansion Extra Dependency
-  "record_expansion.elpi" as record_expansion.
-
-(**
-   This file sketches a procedure to translate a term abstracted over
-   a specific record to a term abstracted over the components of that record.
-
-   Example:
-
-     Record r := mk { proj1 : T1; proj2 : T2 }.
-     Definition f (x : r) := Body(proj1 x,proj2 x).
-
-   Is expanded to:
-
-     Definition f1 v1 v2 := Body(v1,v2).
-
-   And recursively, if g uses f, then g1 must use f1...
-
-   The idea is to take "f", replace "(x : r)" with as many abstractions as
-   needed in order to write "mk v1 v2", then replace "x" with "mk v1 v2", finally
-   fire iota reductions such as "proj1 (mk v1 v2) = v1" to obtain "f1".
-   
-   Then record a global replacement "f x = f1 v2 v2" whenever "x = mk v1 v2".
-
-*)
+From record_expansion Extra Dependency "register.elpi" as register.
+From record_expansion Extra Dependency "expand.elpi" as expand.
 
 Elpi Db record.expand.db lp:{{
-  % This data base will contain all the expansions performed previously.
-  % For example, if f was expandded to f1 we would have this clause:
 
-  % copy (app[f, R | L]) (app[f1, V1, V2 | L1]) :-
-  %   copy R (app[k, V1, V2]), std.map L copy L1.
+% the predicate to accumulate information about the record types to expand
+pred records
+  o:inductive,     % record to expand
+  o:int,           % number of parameters
+  o:constructor,   % record constructor
+  o:term,          % record constructor type
+  o:list constant. % projections
+:name "records:start"
+records _ _ _ _ _ :- fail.
+
+pred expand-const i:constant, o:term.
+:name "expand-const:start"
+expand-const _ _ :- fail.
 
 }}.
 
-Elpi Command record.expand.
+Elpi Command record.register.
 Elpi Accumulate Db record.expand.db.
-Elpi Accumulate File record_expansion.
+Elpi Accumulate File register.
 Elpi Typecheck.
 
-Record r := { T :> Type; X := T; op : T -> X -> bool }.
+Elpi Command record.expand.
+Elpi Accumulate Db record.expand.db.
+Elpi Accumulate File expand.
+Elpi Typecheck.
 
-Definition f b (t : r) (q := negb b) := fix rec (l1 l2 : list t) :=
-  match l1, l2 with
-  | nil, nil => b
-  | cons x xs, cons y ys => andb (op _ x y) (rec xs ys)
-  | _, _ => q
-  end.
+Module Example1.
 
-Elpi record.expand r f "expanded_".
-Print f.
-Print expanded_f.
+Record r := { T : Type; op : T -> T -> T }.
 
-(* so that we can see the new "copy" clause *)
-Elpi Print record.expand.
+Elpi record.register r.
 
-Definition g t l s h := (forall x y, op t x y = false) /\ f true t l s = h.
+Definition r_id (x : r) := x.
 
-Elpi record.expand r g "expanded_".
-Print expanded_g.
+Elpi Query lp:{{ expand-fun `In` {{ r -> r }} _ A }}.
+Elpi Query lp:{{ expand-term _ {{ r -> r }} {{ fun x : r => x }} A }}.
+Elpi Query lp:{{ expand-term _ {{ r -> Type }} {{ fun x : r => T x }} A }}.
+Elpi record.expand r_id "expanded_".
+Print expanded_r_id.
+Print expanded_r_id1.
+
+End Example1.
+
+Module Example2.
+
+Record r1 := { T : Type; op : T -> T -> bool }.
+
+Record r2 := { r2_r1 : r1; refl : forall x, op r2_r1 x x = true }.
+
+Elpi record.register r1.
+Elpi record.register r2.
+
+Elpi Query lp:{{ expand-fun _ {{ r2 }} _ A }}.
+
+End Example2.
